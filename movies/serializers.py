1b463471e2e7b1c554efb467e92104289496634c
Luis <lgmarchioreh@gmail.com>
2023-12-06 20:29:10 -0300

    FEAT(movies) Implemented route for creating new movies

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
-from .models import RatingOpt
-
+from .models import RatingOpt, Movies
+import pdb
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField()
     duration = serializers.CharField(required=False)
-    rating = serializers.CharField(choices=RatingOpt, required=False)
+    rating = serializers.ChoiceField(choices=RatingOpt, required=False)
     synopsis = serializers.CharField()
-    added_by = serializers.CharField(read_only=True)
+    added_by = serializers.CharField(required=False)
     
+    def create(self, validated_data):
+        new_movie = Movies.objects.create(**validated_data, user=self.context['user'])
+        validated_data = new_movie
+        validated_data.added_by = self.context['user'].email
+        validated_data.save()
+        return validated_data
+
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Request, Response, status
+from .serializers import MovieSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 # Create your views here.
+
+class MovieView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def post(self, request: Request) -> Response:
+        user = request.user
+        serialized_movie = MovieSerializer(data=request.data, context={'user': user})
+        serialized_movie.is_valid(raise_exception=True)
+        serialized_movie.save()
+        return Response(serialized_movie.data, status=status.HTTP_201_CREATED)  
