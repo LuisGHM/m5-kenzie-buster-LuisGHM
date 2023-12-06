82645ded3a2509299b3529309c8a847707908b29
Luis <lgmarchioreh@gmail.com>
2023-12-06 11:06:12 -0300

    FEAT(movies) Implemented movie models and app movies

---
+++ movies/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class RatingOpt(models.TextChoices):
+    G = "G"
+    PG = "PG"
+    PG_13 = "PG-13"
+    R = "R"
+    NC_17 = "NC-17"
+
+class Movies(models.Model):
+    title = models.CharField(max_length=127)
+    duration = models.CharField(max_length=10, null=True, default="")
+    rating = models.CharField(max_length=20, choices=RatingOpt.choices, default=RatingOpt.G)
+    synopsis = models.TextField(default="")
+    user = models.ForeignKey("users.User", on_delete=models.PROTECT, related_name="movies")
