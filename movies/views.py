e47a89d43f53df27fac8764fa1a62243cb29d0bc
Luis <lgmarchioreh@gmail.com>
2023-12-07 10:13:55 -0300

    FEAT(movies) Implemented GET route to retrieve all movies from database

--- movies/views.py
+++ movies/views.py
@@ -1,17 +1,24 @@
 from rest_framework.views import APIView, Request, Response, status
 from .serializers import MovieSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from .models import Movies
 
 # Create your views here.
 
 class MovieView(APIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     
     def post(self, request: Request) -> Response:
         user = request.user
         serialized_movie = MovieSerializer(data=request.data, context={'user': user})
         serialized_movie.is_valid(raise_exception=True)
         serialized_movie.save()
-        return Response(serialized_movie.data, status=status.HTTP_201_CREATED)  
+        return Response(serialized_movie.data, status=status.HTTP_201_CREATED)
+
+
+    def get(self, request: Request) -> Response:
+        movies = Movies.objects.all()
+        serializer_movies = MovieSerializer(movies, many=True)
+        return Response(serializer_movies.data)
