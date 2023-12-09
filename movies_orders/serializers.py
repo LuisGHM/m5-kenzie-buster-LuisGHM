513a4a7f5df7503eac0d7df39fcb44ff87efdf31
Luis <lgmarchioreh@gmail.com>
2023-12-09 16:20:17 -0300

    FIX(tests 3) Resolved all identified errors

--- movies_orders/serializers.py
+++ movies_orders/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from .models import MovieOrder
 from movies.models import Movie
+import pdb
 
 class MovieOrderSerializer(serializers.Serializer):
-    id = serializers.CharField(read_only=True)
+    id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(read_only=True)
     purchased_by = serializers.CharField(read_only=True)
     purchased_at = serializers.DateTimeField(read_only=True)
